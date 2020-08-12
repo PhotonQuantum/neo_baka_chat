4e41618d4b789373f8f80e2c6973ccc0f1b4365b
PhotonQuantum <self@lightquantum.me>
2020-08-12 19:46:38 +0800

    fix(utils): half items lost in split_list

--- neo_baka_chat/utils.py
+++ neo_baka_chat/utils.py
@@ -3,6 +3,6 @@ from typing import List
 
 def split_list(seq: list, sep) -> List[list]:
     idx = [-1] + [i for i, x in enumerate(seq) if x == sep] + [len(seq)]
-    slices = [slice(x + 1, y) for x, y in zip(idx[::2], idx[1::2])]
+    slices = [slice(x + 1, y) for x, y in zip(idx[:-1], idx[1:])]
     split_seq = [seq[_slice] for _slice in slices]
     return split_seq
