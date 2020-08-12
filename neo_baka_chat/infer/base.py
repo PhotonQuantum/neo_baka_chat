cc213382674ab5fe2fc4bc7844307775dd7a80cc
PhotonQuantum <self@lightquantum.me>
2020-08-12 14:23:59 +0800

    fix(infer): missing abstract methods

--- neo_baka_chat/infer/base.py
+++ neo_baka_chat/infer/base.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import List
 
 from neo_baka_chat.hparams import BaseHyperParams
@@ -6,10 +6,13 @@ from neo_baka_chat.vocab import Vocab
 
 
 class AbstractInference(ABC):
+    @abstractmethod
     def __init__(self, vocab: Vocab, hparams: BaseHyperParams): ...
 
+    @abstractmethod
     def load_state_dict(self, weights):
         return NotImplemented
 
+    @abstractmethod
     def infer(self, sequence: List[int]) -> List[int]:
         return NotImplemented
