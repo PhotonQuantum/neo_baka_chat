22f7f704e7bf09bf98c5bbb824930e3b3f8f4417
PhotonQuantum <self@lightquantum.me>
2020-08-12 15:52:59 +0800

    fix: forgot to deserialize data before feeding them into trainer (*ﾉωﾉ)

--- neo_baka_chat/train/loader.py
+++ neo_baka_chat/train/loader.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from functools import reduce
 
 from .session import TrainSession
+from ..deserializer import deserialize
 from ..oss import OSS
 
 DISABLE_CUDA = os.environ.get("DISABLE_CUDA") == "true"
@@ -19,11 +20,12 @@ class Config:
 
 def load_session(config: Config, bucket: OSS) -> TrainSession:
     logging.warning("Downloading dataset.")
-    datasets = bucket.list_dataset(config.dataset_prefix)
-    dataset = reduce(lambda x, y: x + y, map(lambda x: x.read(), datasets))
+    datasets = bucket.list_dataset(config.dataset_prefix)[:2]
+    dataset: dict = reduce(lambda x, y: x + y, map(lambda x: x.read(), datasets))
+    messages = deserialize(dataset)
 
     logging.warning(f"Loading net {config.model}.")
-    session = TrainSession(dataset, config.hparams, config.model, not DISABLE_CUDA)
+    session = TrainSession(messages, config.hparams, config.model, not DISABLE_CUDA)
 
     logging.warning("Standby.")
     return session
